c9932891360264b5898342542216321039f4dd98
MYHT\My <myht@runsystem.net>
2018-01-02 13:35:35 +0700

    up product

---
+++ pruduct/assets/scripts/common.js
@@ -0,0 +1,48 @@
+$(document).ready(function () {
+    $(document).on('click', '.video img', function (event) {
+        $(event.target).closest('p').append('<iframe width="100%" height="100%" src="https://www.youtube.com/embed/jlDDxg7gxLU?autoplay=1" frameborder="0" allowfullscreen></iframe>');
+        $(event.target).remove();
+    });
+    //page top
+    $('.page-top').click(function() {
+        $('html, body').animate({scrollTop:0}, 1000);
+    });
+
+    // $(document).scroll(function(e) {
+    //   var headerHeight = $('.header-language').height();
+    //   if($(window).scrollTop() > headerHeight) {
+    //         $('.header-language').hide();
+    //         $('.header ').addClass('header-fix')
+    //     } else {
+    //         $('.header-language').show();
+    //         $('.header ').removeClass('header-fix')
+    //     }
+    // });
+    // var mySlider = $('.pogoSlider').pogoSlider({
+    //     pauseOnHover: false
+    // }).data('plugin_pogoSlider');
+    $('.search-js').on('click', function () {
+        if($(this).hasClass('search-active')) {
+            $(this).removeClass('search-active')
+            $('.search-form-js').slideUp()
+        } else {
+            $(this).addClass('search-active')
+            $('.search-form-js').slideDown()
+        }
+    })
+    //meanmenu
+    $('.header-menu-content-js').meanmenu( {
+        meanScreenWidth: "768"
+    });
+    /*
+    Scroll product page
+     */
+    function scrollTo(el) {
+        var element = $(el);
+        $('html,body').animate({scrollTop: element.offset().top},'slow');
+    }
+    $('.menu-product-js li a').on('click',function () {
+        var ele = $(this).attr('href');
+        scrollTo(ele);
+    })
+})
