ce3a8a63059ca4fbe705ec20f33ede199e8cabde
my <my.thaohoang@gmail.com>
2018-01-07 19:04:56 +0700

    code release

--- src/assets/scripts/common.js
+++ src/assets/scripts/common.js
@@ -7,20 +7,6 @@ $(document).ready(function () {
     $('.page-top').click(function() {
         $('html, body').animate({scrollTop:0}, 1000);
     });
-
-    // $(document).scroll(function(e) {
-    //   var headerHeight = $('.header-language').height();
-    //   if($(window).scrollTop() > headerHeight) {
-    //         $('.header-language').hide();
-    //         $('.header ').addClass('header-fix')
-    //     } else {
-    //         $('.header-language').show();
-    //         $('.header ').removeClass('header-fix')
-    //     }
-    // });
-    // var mySlider = $('.pogoSlider').pogoSlider({
-    //     pauseOnHover: false
-    // }).data('plugin_pogoSlider');
     $('.search-js').on('click', function () {
         if($(this).hasClass('search-active')) {
             $(this).removeClass('search-active')
@@ -53,5 +39,21 @@ $(document).ready(function () {
         } else {
             $('.header-language').show();
             $('.header ').removeClass('header-fix')
-        }});
+        }
+        var top = $('.banner').height();
+        if($(window).width() >= 1170) {
+            if($(window).scrollTop() > top) {
+                $('.menu-product-js').addClass('menu-product-scroll');
+            } else {
+                $('.menu-product-js').removeClass('menu-product-scroll');
+            }
+        }
+
+        if($(window).scrollTop() > 400 ) {
+            $('.page-top').fadeIn();
+        } else {
+            $('.page-top').fadeOut();
+        }
+    });
+
 })
